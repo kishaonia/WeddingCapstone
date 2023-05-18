1afab9f6337f15fa0eb7d211e145a46fd5298776
kisha onia <kisharoseonia@gmail.com>
2023-05-18 05:17:43 -0700

    fixed install command

--- frontend/src/components/WeddingDetails/index.js
+++ frontend/src/components/WeddingDetails/index.js
@@ -1,12 +1,22 @@
 import React from "react";
 import "./WeddingDetails.css";
-import weddingdetailsbg from "../../assets/weddingdetailsbg.jpg"
+import weddingdetailsbg from "../../assets/weddingdetailsbg.jpg";
 
 const WeddingDetails = () => {
   return (
     <div className="wedding-details-header">
-      <img src={weddingdetailsbg} height="800px" width="1700px" />
+      <img src={weddingdetailsbg} height="800px" width="1700px" alt="Wedding Background" />
+      <div className="overlay-wedding-details-header">
+        <div className="text-on-overlay-wedding-details-header">
+          <h2>Our Wedding</h2>
+          <p>December xx, 2023</p>
+          <p>We can't wait to celebrate with you!</p>
+        </div>
+      </div>
+      
     </div>
+   <div className="lol">lol
+   </div>
   );
 };
 
