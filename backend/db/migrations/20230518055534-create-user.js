a75af9b6c0fddbe482206c058189add84bc1b538
kisha onia <kisharoseonia@gmail.com>
2023-05-18 00:53:25 -0700

    fixed migrations

--- backend/db/migrations/20230518055534-create-user.js
+++ backend/db/migrations/20230518055534-create-user.js
@@ -1,5 +1,10 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
+let options = {};
+
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Users', {
@@ -35,9 +40,10 @@ module.exports = {
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
 
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
+    options.tableName = "Users";
     await queryInterface.dropTable('Users');
   }
 };
--- backend/db/migrations/20230518060134-create-photo.js
+++ backend/db/migrations/20230518060134-create-photo.js
@@ -1,5 +1,10 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
+let options = {};
+
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Photos', {
@@ -34,9 +39,10 @@ module.exports = {
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
 
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
+      options.tableName = "Comments";
     await queryInterface.dropTable('Photos');
   }
 };
